b5c8c5b202cd7727ef37b4f3fde16f37e75eb949
Adam <1392689+coffeegist@users.noreply.github.com>
2024-12-20 09:22:38 -0600

    * Use importlib to support python3.12; Update scapy

--- common/orchestra.py
+++ common/orchestra.py
@@ -5,7 +5,7 @@ This is the conductor which controls everything
 '''
 
 import glob
-import imp
+import importlib as imp
 from commandcontrol.malware import *
 from commandcontrol.apt import *
 from protocols.servers import *
@@ -25,32 +25,45 @@ class Conductor:
 
     def load_client_protocols(self, command_line_object):
         for name in glob.glob('protocols/clients/*.py'):
+            #print(name)
             if name.endswith(".py") and ("__init__" not in name):
-                loaded_client_proto = imp.load_source(name.replace("/", ".").rstrip('.py'), name)
+                #loaded_client_proto = imp.load_source(name.replace("/", ".").rstrip('.py'), name)
+                s_loaded_client_proto = imp.util.spec_from_file_location("." + name.replace("/", ".").rstrip('.py'), name)
+                loaded_client_proto = imp.util.module_from_spec(s_loaded_client_proto)
+                s_loaded_client_proto.loader.exec_module(loaded_client_proto)
+                #print((loaded_client_proto.__dict__))
                 self.client_protocols[name] = loaded_client_proto.Client(command_line_object)
         return
 
     def load_server_protocols(self, command_line_object):
         for name in glob.glob('protocols/servers/*.py'):
             if name.endswith(".py") and ("__init__" not in name):
-                loaded_server_proto = imp.load_source(name.replace("/", ".").rstrip('.py'), name)
+                spec = imp.util.spec_from_file_location(name.replace("/", ".").rstrip('.py'), name)
+                loaded_server_proto = imp.util.module_from_spec(spec)
+                spec.loader.exec_module(loaded_server_proto)
                 self.server_protocols[name] = loaded_server_proto.Server(command_line_object)
         return
 
     def load_datatypes(self, command_line_object):
         for name in glob.glob('datatypes/*.py'):
             if name.endswith(".py") and ("__init__" not in name):
-                loaded_datatypes = imp.load_source(name.replace("/", ".").rstrip('.py'), name)
+                spec = imp.util.spec_from_file_location(name.replace("/", ".").rstrip('.py'), name)
+                loaded_datatypes = imp.util.module_from_spec(spec)
+                spec.loader.exec_module(loaded_datatypes)
                 self.datatypes[name] = loaded_datatypes.Datatype(command_line_object)
         return
 
     def load_actors(self, command_line_object):
         for name in glob.glob('commandcontrol/malware/*.py'):
             if name.endswith(".py") and ("__init__" not in name):
-                loaded_actors = imp.load_source(name.replace("/", ".").rstrip('.py'), name)
+                spec = imp.util.spec_from_file_location(name.replace("/", ".").rstrip('.py'), name)
+                loaded_actors = imp.util.module_from_spec(spec)
+                spec.loader.exec_module(loaded_actors)
                 self.actor_modules[name] = loaded_actors.Actor(command_line_object)
         for name in glob.glob('commandcontrol/apt/*.py'):
             if name.endswith(".py") and ("__init__" not in name):
-                loaded_actors = imp.load_source(name.replace("/", ".").rstrip('.py'), name)
+                spec = imp.util.spec_from_file_location(name.replace("/", ".").rstrip('.py'), name)
+                loaded_actors = imp.util.module_from_spec(spec)
+                spec.loader.exec_module(loaded_actors)
                 self.actor_modules[name] = loaded_actors.Actor(command_line_object)
         return
